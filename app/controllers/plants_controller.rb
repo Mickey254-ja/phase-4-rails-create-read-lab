1078ae004459a89935d681a4269944f72d6752a9
Mickey254-ja <mickey.awino@student.moringaschool.com>
2023-03-16 15:19:02 +0300

    completed assignment

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,32 @@
+class PlantsController < ApplicationController
+    #To disable the wrap feature
+    wrap_parameters format:[]
+    
+    
+    #GET /plants
+    def index
+        plants = Plant.all
+        render json:plants, except: [:created_at, :updated_at]
+    end
+
+    #GET plants /:id
+    def show 
+    plant = Plant.find(params[:id])
+        # if plant
+            render json: plant
+        # else 
+        #     render json: {error:"Plant not found"}, status: :not found
+        # end
+    end
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+    private
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+end
